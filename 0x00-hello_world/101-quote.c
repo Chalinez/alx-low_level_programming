89e479899bb13e3b6c6fd1a1bd69035abff726da
Paschaline Anekwe <chalinez213@gmail.com>
2022-04-07 12:05:56 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 	*main- entry point
 	*
-	*Return: 1 after printing function
+	*Return: Always 0 (Success)
 	*/
 int main(void)
 {
-	char myLetter = 'and that piece of art is useful - Dora Korpar';
-	int myNum = 2015-10-19
-		putchar("%d", myNum);
-		putchar("%c\n", myLetter);
-	return (1)
+	write(1, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",59);
+	return (1);
 }
-
