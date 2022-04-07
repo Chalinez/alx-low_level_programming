2b9b7b6ddeb9b75b67cae19ac6487ab5cdc776b2
Paschaline Anekwe <chalinez213@gmail.com>
2022-04-07 11:06:49 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+	*main- entry point
+	*
+	*Return: 1 after printing function
+	*/
+int main(void)
+{
+	char myLetter = 'and that piece of art is useful - Dora Korpar';
+	int myNum = 2015-10-19
+		putchar("%d", myNum);
+		putchar("%c\n", myLetter);
+	return (1)
+}
+
