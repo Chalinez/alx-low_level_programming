e24067e7324a3280e6200c05594b27b859fc33ea
Paschaline Anekwe <chalinez213@gmail.com>
2022-04-07 08:22:02 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main- entry point
- *
- *return- will return 0 
- */
+	*main- entry point
+	*
+	*return- will return 0
+	*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
