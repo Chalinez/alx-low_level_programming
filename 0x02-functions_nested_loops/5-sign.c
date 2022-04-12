e85b62b226bc54f7afac4f0fdf133b347df39a95
Paschaline Anekwe <chalinez213@gmail.com>
2022-04-12 03:57:34 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+	*print-sign- return 0 not lowercase, 1 letter lowercase
+	*
+	*@n: int to be printed
+	*Return: Always 0
+	*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+	_putchar ('+');
+	return (1);
+}
+else if (n == 0)
+{
+		_putchar ('0');
+		return (0);
+}
+else
+	_putchar ('-');
+	return (-1);
+}
+
+}
+
