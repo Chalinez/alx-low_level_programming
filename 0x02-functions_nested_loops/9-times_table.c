2e230cc23b94df847ea583270b571631312efbaa
Paschaline Anekwe <chalinez213@gmail.com>
2022-04-12 04:57:24 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,10 +12,8 @@ int b;
 int c;
 for (a = 0; a <= 9; a++)
 {
-
 for (b = 0; b <= 9; b++)
 {
-
 c = a * b;
 if ((c / 10) == 0)
 {
@@ -40,7 +38,6 @@ _putchar ((c / 10) + '0');
 _putchar ((c % 10) + '0');
 if (b < 9)
 {
-
 	_putchar(',');
 	_putchar (' ');
 }
