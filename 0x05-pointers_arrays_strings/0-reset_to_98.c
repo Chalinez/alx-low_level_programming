8301a98effcdb8a2bc01b6643044275a9dad4c04
Paschaline Anekwe <chalinez213@gmail.com>
2022-04-20 09:28:59 -0700

    My commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,9 @@
-#include "main.h" 
-
+#include "main.h"
  /** 
-
-     * reset_to_98 - reset argument to 98 
-
-       * @n: pointer 
-
-         * Return: void 
-
-	  */ 
+     *reset_to_98 - reset argument to 98
+     *@n: pointer
+     *Return: void
+     */
 
 void reset_to_98(int *n)
 {
