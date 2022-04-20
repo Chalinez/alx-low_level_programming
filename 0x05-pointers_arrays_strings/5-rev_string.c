1201e30188639c35761ccccfa77b8eefb172baa8
Paschaline Anekwe <chalinez213@gmail.com>
2022-04-20 10:17:21 -0700

    My commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,15 @@ void rev_string(char *s)
 	int len, i, half;
 	char temp;
 for (len = 0; s[len] != '\0'; len++)
+{
 	i = 0;
 	half = len / 2;
+}
 while (half--)
 {
 	temp = s[len - i - 1];
 	s[len - i - 1] = s[i];
 	s[i] = temp;
-	i++
+	i++;
 }
 }
